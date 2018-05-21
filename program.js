19ed1286047951d760cff00f6d86ab812c8beb2f
Tobiasz18 <tobiaszpost@gmail.com>
2018-05-21 17:44:03 +0200

    os module

--- program.js
+++ program.js
@@ -1,12 +1,15 @@
-const os = require('os');
+const osInfo = require('./OS');
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', () => {
     const input = process.stdin.read();
-    console.log(os)
+
     if(input !== null) {
         const instruction = input.toString().trim();
         switch(instruction) {
+            case '/osInfo':
+                osInfo.print();
+                break;
             case '/node':
                 process.stdout.write(process.versions.node + '\n');
                 break;
