177d22bde31b0c60a8d4f5b4335dd6acb6b51d5c
Tobiasz18 <tobiaszpost@gmail.com>
2018-05-22 19:01:55 +0200

    events

--- program.js
+++ program.js
@@ -1,5 +1,16 @@
 const osInfo = require('./OS');
 const colors = require('colors');
+const EventEmitter = require('events').EventEmitter;
+
+const emitter = new EventEmitter();
+
+emitter.on('beforeCommand', (instruction) => {
+    console.log('You wrote: ' + instruction + ' trying to run');
+});
+
+emitter.on('afterCommand', () => {
+    console.log('Finished command');
+});
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', () => {
@@ -7,6 +18,8 @@ process.stdin.on('readable', () => {
 
     if(input !== null) {
         const instruction = input.toString().trim();
+        emitter.emit('beforeCommand', colors.red(instruction));
+
         switch(instruction) {
             case '/osInfo':
                 osInfo.print();
@@ -24,5 +37,6 @@ process.stdin.on('readable', () => {
             default:
                 process.stderr.write('Wrong instruction \n')
         }
+        emitter.emit('afterCommand');
     }
 });
